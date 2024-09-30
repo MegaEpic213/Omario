5ab339d4264582c5ab8a1c2a3878e248ed7659b0
Omar.Abdul <omarabdul471@gmil.com>
2024-09-30 17:24:36 +0500

    Выполнено

---
+++ Sincos/Sincos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sincos
+{
+    
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double angleinDegrees = 15;
+            double angleinRadians = angleinDegrees * Math.PI / 180;
+            var sin = Math.Sin(angleinRadians);  
+            var cos = Math.Cos(angleinRadians);
+            Console.WriteLine("sin(15\xB0) = " + Math.Round(sin,3));
+            Console.WriteLine("cos(15\xB0) = " + Math.Round(cos, 3));
+
+             angleinDegrees = 37;
+             angleinRadians = angleinDegrees * Math.PI / 180;
+             sin = Math.Sin(angleinRadians);
+             cos = Math.Cos(angleinRadians);
+            Console.WriteLine("sin(37\xB0) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(37\xB0) = " + Math.Round(cos, 3));
+
+            angleinDegrees = 113;
+            angleinRadians = angleinDegrees * Math.PI / 180;
+            sin = Math.Sin(angleinRadians);
+            cos = Math.Cos(angleinRadians);
+            Console.WriteLine("sin(113\xB0) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(113\xB0) = " + Math.Round(cos, 3));
+
+            Console.WriteLine();
+            Console.WriteLine("Введите угол в градусах:");
+            angleinDegrees = double.Parse(Console.ReadLine());
+            angleinRadians = angleinDegrees * Math.PI / 180;
+            sin = Math.Sin(angleinRadians);
+            cos = Math.Cos(angleinRadians);
+            Console.WriteLine("sin(" + angleinDegrees + "\xB0) = " + Math.Round(sin, 3));
+            Console.WriteLine("cos(" + angleinDegrees + "\xB0) = " + Math.Round(cos, 3));
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину большого основания трапеции:");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину малого основания трапеции:");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите угол при основании (в градусах):");
+            var alpha = double.Parse(Console.ReadLine()) * Math.PI / 180;
+            var x = (a + b) / 2;
+            var h = x * Math.Tan(alpha);
+            var s = (a + b) * h / 2;
+            Console.WriteLine("Площадь трапеции: " + s);
+
+            var y =x/Math.Cos(alpha);
+            var p = 2 * y + a + b;
+            Console.WriteLine("Периметр трапеции: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
